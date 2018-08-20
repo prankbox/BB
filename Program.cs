54f5bf3d24d408428d4135f07d922e5b6d2127db
prankbox <edelweissco@mail.ru>
2018-08-20 19:56:36 +0300

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace BB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
         }
     }
 }
